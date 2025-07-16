f708f0ec1afc9638cc367d6f76d4a6c7edd0570d
andrr64 <derzandrr03@gmail.com>
2025-07-16 23:50:38 +0700

    feat: update Home page to redirect to ROUTES.HOME and replace content with loading component

--- keuanganku/app/page.tsx
+++ keuanganku/app/page.tsx
@@ -1,17 +1,19 @@
 'use client';
-import { useTheme } from "next-themes";
-import Image from "next/image";
+
+import Loading from "@/components/Loading";
+import { ROUTES } from "@/lib/routes";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
 
-  const {theme, setTheme} = useTheme();
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push(ROUTES.HOME)
+  }, []);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900 transition-all duration-300">
-      <div>
-        <h1>Next.JS dark mode </h1>
-        <button onClick={() => setTheme(theme === "light"? "dark" : "light")}>Dark/Light</button>
-      </div>
-    </div>
+    <Loading/>
   );
 }
