47d6cb0c3edcd41b9a264e0e73cdd90c2d00bad1
andrr64 <derzandrr03@gmail.com>
2025-07-16 23:49:18 +0700

    feat: add account management actions for adding, retrieving, and deleting accounts

---
+++ keuanganku/actions/akun.ts
@@ -0,0 +1,23 @@
+import { API_ROUTES } from "@/lib/api";
+import { fetcher } from "@/lib/fetcher";
+import { AkunResponse } from "@/types/akun";
+
+export const tambahAkun = async (nama: string, saldoAwal: number) => {
+    return fetcher(API_ROUTES.AKUN.TAMBAH, {
+        method: 'POST',
+        body: JSON.stringify({ namaAkun: nama, saldoAwal }),
+    });
+}
+
+export const getAllAkun = async () => {
+    return fetcher<AkunResponse[]>(API_ROUTES.AKUN.GET_SEMUA, {
+        method: 'GET',
+    });
+}
+
+export const hapusAkun = async (id: string) => {
+    return fetcher(API_ROUTES.AKUN.HAPUS(id), {
+        method: 'DELETE',
+        body: JSON.stringify({ idAkun: id }),
+    });
+}
