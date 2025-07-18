bf9e6dbb910d7cdf233b4ffced22ba2f89664696
andrr64 <derzandrr03@gmail.com>
2025-07-18 14:31:25 +0700

    feat: menambahakn parameter baru

--- keuanganku/actions/transaksi.ts
+++ keuanganku/actions/transaksi.ts
@@ -1,5 +1,6 @@
 import { API_ROUTES } from '@/lib/api';
 import { fetcher } from '@/lib/fetcher';
+import { TransaksiResponse } from '@/types/transaksi';
 
 export interface TambahTransaksiParams {
   idKategori: string;
@@ -9,9 +10,27 @@ export interface TambahTransaksiParams {
   catatan?: string;
 }
 
+export interface AmbilTransaksiParams {
+  startDate?: string;
+  endDate?: string;
+  jenis?: number;
+  idAkun?: string;
+  page?: number;
+  size?: number;
+  search?: string;
+}
+
+export interface DataGetTransaksi {
+  totalItems: number;
+  totalPages: number;
+  currentPage: number;
+  content: TransaksiResponse[]
+  success: boolean;
+}
+
 export async function tambahTransaksi(params: TambahTransaksiParams) {
   return fetcher(
-    API_ROUTES.TRANSAKSI.TAMBAH, 
+    API_ROUTES.TRANSAKSI.TAMBAH,
     {
       method: 'POST',
       headers: {
@@ -26,4 +45,20 @@ export async function tambahTransaksi(params: TambahTransaksiParams) {
       }),
     }
   );
+}
+
+export async function ambilTransaksi(params: AmbilTransaksiParams) {
+  const url = new URL(API_ROUTES.TRANSAKSI.FILTER);
+
+  if (params.startDate) url.searchParams.set("startDate", params.startDate);
+  if (params.endDate) url.searchParams.set("endDate", params.endDate);
+  if (params.jenis) url.searchParams.set("jenis", String(params.jenis));
+  if (params.idAkun) url.searchParams.set("idAkun", params.idAkun);
+  if (params.page !== undefined) url.searchParams.set("page", String(params.page));
+  if (params.size !== undefined) url.searchParams.set("size", String(params.size));
+  if (params.search) url.searchParams.set("keyword", params.search);
+
+  return fetcher<DataGetTransaksi | null>(url.toString(), {
+    method: "GET"
+  });
 }
