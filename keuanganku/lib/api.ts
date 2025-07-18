f881ec08e2f77bc43099f347b1dcacd078437046
andrr64 <derzandrr03@gmail.com>
2025-07-18 14:31:53 +0700

    fix: meperbarui endpoint

--- keuanganku/lib/api.ts
+++ keuanganku/lib/api.ts
@@ -10,7 +10,7 @@ export const API_ROUTES = {
     TAMBAH: `${API_BASE_URL}/secure/akun`, // POST
     GET_SEMUA: `${API_BASE_URL}/secure/akun`, // GET
     UPDATE_NAMA: (idAkun: string) => `${API_BASE_URL}/secure/akun/update-nama/${idAkun}`, // PUT
-    HAPUS:(idAkun: string) => `${API_BASE_URL}/secure/akun/${idAkun}`, // DELETE
+    HAPUS: (idAkun: string) => `${API_BASE_URL}/secure/akun/${idAkun}`, // DELETE
   },
 
   KATEGORI: {
@@ -20,6 +20,7 @@ export const API_ROUTES = {
   },
 
   TRANSAKSI: {
-    TAMBAH: `${API_BASE_URL}/secure/transaksi`
+    TAMBAH: `${API_BASE_URL}/secure/transaksi`,
+    FILTER: `${API_BASE_URL}/secure/transaksi`
   }
 };
